304af0ae581a142f66ab949a6b5266bec83e733e
Vianey Chavez <viantage@gmail.com>
2024-08-22 23:17:07 -0600

    mejora de la codificación  y la transformación

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#World Crops
+# World Crops
 
 Ejercicio de ETL. 
 
@@ -6,9 +6,9 @@ Ejercicio de ETL.
 _https://www.kaggle.com/datasets/imtkaggleteam/crop-production/data_
 
 **Librerias:**
-*numpy==2.0.1
-*pandas==2.2.2
-*python-dateutil==2.9.0.post0
-*pytz==2024.1
-*six==1.16.0
-*tzdata==2024.1
+- numpy==2.0.1
+- pandas==2.2.2
+- python-dateutil==2.9.0.post0
+- pytz==2024.1
+- six==1.16.0
+- tzdata==2024.1
--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 import pandas as pd
 import os
 
+#Leer archivos
 def read_files(file_paths):
     dataframes = {}
     for key, path in file_paths.items():
-        dataframes[key] = pd.read_csv(path, encoding='ISO-8859-1')
+        dataframes[key] = pd.read_csv(path, encoding='windows-1252')
     return dataframes
 
+#Identificar nulos
 def explore_files(df):
     valores_nulos = df.isna().sum()
     print("Valores atípicos", valores_nulos)
     return df
 
+#Concatenar archivos
 def concat_data(dfs):
     combined_df = pd.concat(dfs)
     return combined_df
 
+#Ajustar archivo, reenombrar continentes
+def transform_data(df):
+    continentes = {
+        'africa' : 'África',
+        'america' : 'Ámerica',
+        'asia' : 'Asia',
+        'europa' : 'Europa',
+        'oceania' : 'Oceanía'
+    }
+    df['continente'] = df['continente'].map(continentes)
+
+    columns_to_remove = [col for col in df.columns if col.endswith('F')]
+    df = df.drop(columns=columns_to_remove)
+
+    return df
+
+#Cargar archivo
 def load_processed_data(df, output_path):
-    
     if not os.path.exists(os.path.dirname(output_path)):
         os.makedirs(os.path.dirname(output_path))
 
-    df.to_csv(output_path, index=False)
+    df.to_csv(output_path, index=False, encoding='windows-1252')
 
-
+#Rutas a los archivos
 file_paths ={
     'africa' : r'raw\\Production_Crops_E_Africa.csv',
     'america' : r'raw\\Production_Crops_E_Americas.csv',
@@ -32,16 +51,24 @@ file_paths ={
     'oceania' : r'raw\\Production_Crops_E_Oceania.csv'    
 }
 
+#Llama a leer archivos
 dataframes = read_files(file_paths)
 
-transformed_dataframes = {name: explore_files(
+#Crea diccionario
+e_dataframes = {name: explore_files(
     df) for name, df in dataframes.items()}
 
+#Agreaga columna con continente
 dataframes_with_keys = [
-    df.assign(source_file=key) for key, df in transformed_dataframes.items()
+    df.assign(continente=key) for key, df in e_dataframes.items()
 ]
 
+#Llama a concatenar df
 df_combined = concat_data(dataframes_with_keys)
-print(df_combined)
 
-load_processed_data(df_combined, "processed_data/combined_data.csv")
+#Llama a la función par transformar el df final
+df_final = transform_data(df_combined)
+print(df_final)
+
+#Carga el producto de la transformación
+load_processed_data(df_final, "processed_data/combined_data.csv")
